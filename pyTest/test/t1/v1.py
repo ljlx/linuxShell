092800b8e8727e62c69ce7f1afdba55f1730b934
hx940929 <836845967@qq.com>
2018-08-23 00:21:58 +0800

    py-test

---
+++ pyTest/test/t1/v1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# coding=utf-8
+# --------------------------------------------------
+# File Name: 
+# Author: hanxu
+# AuthorSite: https://www.thesunboy.com/
+# Created Time: 2018-08-22 下午10:40
+# ---------------------说明--------------------------
+#  
+# ---------------------------------------------------
+
+ii = raw_input()
+jj = raw_input()
+sumij = ii + jj;
+
+if sumij == 10:
+    print('yes');
+else:
+    print('no:' + sumij);
+    print('no:', sumij);
+    print('s'
+          'ss')
