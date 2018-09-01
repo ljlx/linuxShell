1c005f1b3864640496b659ed50b33bc00b642eca
hx940929 <836845967@qq.com>
2018-09-01 18:32:05 +0800

    py，字典操作demo,使用列表来做条件索引,字典单纯用作统计,使用setdefault来新增字典节点

--- pyTest/test/t3/headFirst2/chapter/c3-p120.py
+++ pyTest/test/t3/headFirst2/chapter/c3-p120.py
@@ -10,12 +10,14 @@
 # ---------------------------------------------------
 # 先使用初始化，后面在动态扩张i，o，u的统计信息
 # vowels={}
-vowels = {'o': 10, 'u': 10}
-vowels['o'] = 0
-vowels['u'] = 0
-vowels.setdefault('i', 0)
-vowels.setdefault('e', 0)
-vowels.setdefault('a', 0)
+vowels = ['a', 'e', 'i', 'o', 'u']
+# found = {'o': 10, 'u': 10}
+# found['o'] = 0
+# found['u'] = 0
+found = {}
+# found.setdefault('i', 0)
+# found.setdefault('e', 0)
+# found.setdefault('a', 0)
 isstop = False
 while not isstop:
     text = input("请输入待测试的值:")
@@ -25,9 +27,10 @@ while not isstop:
         break
     for item in list(text):
         if (item in vowels):
-            vowels[item] += 1
+            found.setdefault(item, 0)
+            found[item] += 1
             print("找到一个元音：" + item)
 log = "volwel:{0}, counts:{1}"
 # 打印统计结果
-for itemkey, itemvalue in sorted(vowels.items()):
+for itemkey, itemvalue in sorted(found.items()):
     print(log.format(itemkey, itemvalue))
