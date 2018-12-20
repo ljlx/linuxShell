15fbaba4e1ec5de422ebef95e8154b558da61d0a
hanxu <940274999@qq.com>
2018-12-20 14:32:52 +0800

    py-redis操作 githook测试.

---
+++ pyTest/test/githook/1.hook-test1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# coding=utf-8
+# --------------------------------------------------
+# File Name: 
+# Author: hanxu
+# AuthorSite: https://www.thesunboy.com/
+# Created Time: 2018-12-20 下午2:20
+# ---------------------说明--------------------------
+#  
+# ---------------------------------------------------
+
+# from redis import Redis
+
+# redcli = Redis(host='localhost', port=10000)
+import os
+
+mkresu = os.mkdir(path='/tmp/githook/testhook/pyhook/')
+print(mkresu)
