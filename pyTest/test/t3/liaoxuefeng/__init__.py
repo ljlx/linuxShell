9ed0381f93e6f014b7d1258c5572a76b972b23e4
hanxu <940274999@qq.com>
2019-01-18 17:37:41 +0800

    [py]-发现py文件命名中,若是有-号,则在其他文件中无法导入其函数. 使用其操作函数来批量重命名操作.

--- pyTest/test/t3/liaoxuefeng/__init__.py
+++ pyTest/test/t3/liaoxuefeng/__init__.py
@@ -8,3 +8,46 @@
 # ---------------------说明--------------------------
 #  
 # ---------------------------------------------------
+import os, logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s [%(lineno)d] %(levelname)s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S'
+                    )
+logger = logging.getLogger("rename-py")
+
+
+def findCurrDirPyFile(path=".") -> list:
+    pylist = os.listdir(path)
+    # [for item in pylist if os.path.isfile(item) and os.path.split(item)]
+    pyfileNameList = []
+    for item in pylist:
+        isfile = os.path.isfile(item)
+        filetext = os.path.splitext(item)
+        if isfile and len(filetext) == 2:
+            filename = filetext[0]
+            fileSuffix = filetext[1]
+            if ".py" == fileSuffix:
+                pyfileNameList.append(filename + ".py")
+    # print(pyfileNameList)
+    return pyfileNameList
+
+
+def dealWithPyList(pylist: list):
+    for item in pylist:
+        if isinstance(item, str):
+            newpyname = item.replace("-", "_")
+            logtext = "dealWithPyList()==>rename: [{}] -> [{}], result----[%s]".format(item, newpyname)
+            logresult = "----[OK]"
+            iserror = False
+            try:
+                result = os.rename(item, newpyname)
+                logger.debug("dealWithPyList()==>" + logtext, result)
+            except BaseException as ex:
+                print(ex)
+                logger.error("dealWithPyList()==>" + logtext, "Fail,the info:")
+
+
+if __name__ == '__main__':
+    pylist = findCurrDirPyFile()
+    dealWithPyList(pylist)
