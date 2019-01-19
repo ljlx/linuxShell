2ac8cd70f4221c7fb3bce37da79c4b9fc252634a
hanxu <940274999@qq.com>
2019-01-19 11:49:55 +0800

    [py]-分布式进程-server-client

--- pyTest/test/t3/liaoxuefeng/p12_t5_distributedProcess_1.py
+++ pyTest/test/t3/liaoxuefeng/p12_t5_distributedProcess_1.py
@@ -7,7 +7,7 @@
 # GitSource: https://github.com/hx940929/linuxShell
 # Created Time: 2019-1-18-下午4:33
 # ---------------------说明--------------------------
-# 分布式进程学习
+# 分布式进程学习-生产端-server
 # ---------------------------------------------------
 # https://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000/001431929340191970154d52b9d484b88a7b343708fcc60000#0
 
@@ -24,3 +24,41 @@ result_queue = queue.Queue()
 
 class MyQueueManager(BaseManager):
     pass
+
+
+MyQueueManager.register('get_task_queue', callable=lambda: task_queue)
+MyQueueManager.register('get_result_queue', callable=lambda: result_queue)
+
+# 绑定端口5000, 设置验证码'abc':
+manager = MyQueueManager(address=('', 5000), authkey=b'abc')
+# 启动Queue:
+manager.start()
+
+
+# 获得通过网络访问的Queue对象:
+def packagQueue(qu) -> queue.Queue:
+    """
+    请注意，当我们在一台机器上写多进程程序时，创建的Queue可以直接拿来用，但是，在分布式多进程环境下，添加任务到Queue不可以直接对原始的task_queue进行操作，那样就绕过了QueueManager的封装，
+    必须通过manager.get_task_queue()获得的Queue接口添加。
+    :param qu:
+    :return:
+    """
+    return qu
+
+
+task = packagQueue(manager.get_task_queue())
+result = packagQueue(manager.get_result_queue())
+
+# 放几个任务进去:
+for i in range(10):
+    n = random.randint(0, 10000)
+    print('Put task %d...' % n)
+    task.put(n)
+# 从result队列读取结果:
+print('Try get results...')
+for i in range(10):
+    r = result.get()
+    print('Result: %s' % r)
+# 关闭:
+manager.shutdown()
+print('master exit.')
---
+++ pyTest/test/t3/liaoxuefeng/p12_t5_distributedProcess_2.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# --------------------------------------------------
+# File Name: p12_t5_distributedProcess_2.py
+# Author: hanxu
+# AuthorSite: http://www.thesunboy.com/
+# GitSource: https://github.com/hx940929/linuxShell
+# Created Time: 2019-1-19-上午11:38
+# ---------------------说明--------------------------
+# 分布式进程学习-消费端-client
+# ---------------------------------------------------
+# 然后，在另一台机器上启动任务进程（本机上启动也可以）：
+
+import time, sys, queue
+from multiprocessing.managers import BaseManager
+
+
+# import pyTest.test.t3.liaoxuefeng.p12_t5_distributedProcess_1 as myp12d1
+
+
+# 创建类似的QueueManager:
+class MyQueueManager(BaseManager):
+    pass
+
+
+# 由于这个QueueManager只从网络上获取Queue，所以注册时只提供名字:
+MyQueueManager.register('get_task_queue')
+MyQueueManager.register('get_result_queue')
+
+# 连接到服务器，也就是运行task_master.py的机器:
+server_addr = '127.0.0.1'
+print('Connect to server %s...' % server_addr)
+# 端口和验证码注意保持与task_master.py设置的完全一致:
+m = MyQueueManager(address=(server_addr, 5000), authkey=b'abc')
+# 从网络连接:
+m.connect()
+# 获取Queue的对象:
+# task = myp12d1.packagQueue(m.get_task_queue())
+# result = myp12d1.packagQueue(m.get_result_queue())
+task = m.get_task_queue()
+result = m.get_result_queue()
+
+# 从task队列取任务,并把结果写入result队列:
+for i in range(10):
+    try:
+        n = task.get(timeout=1)
+        print('run task %d * %d...' % (n, n))
+        r = '%d * %d = %d' % (n, n, n * n)
+        time.sleep(1)
+        result.put(r)
+    except queue.Queue.Empty:
+        print('task queue is empty.')
+# 处理结束:
+print('worker exit.')
