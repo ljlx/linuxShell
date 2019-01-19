aab412205fb80634dd691d59ad49b86ad9932304
hanxu <940274999@qq.com>
2019-01-19 18:41:56 +0800

    [py]-正则.

--- pyTest/test/t3/liaoxuefeng/p12_t5_distributedProcess_2.py
+++ pyTest/test/t3/liaoxuefeng/p12_t5_distributedProcess_2.py
@@ -28,10 +28,10 @@ MyQueueManager.register('get_task_queue')
 MyQueueManager.register('get_result_queue')
 
 # 连接到服务器，也就是运行task_master.py的机器:
-server_addr = '127.0.0.1'
+server_addr = 'hk.thesunboy.com'
 print('Connect to server %s...' % server_addr)
 # 端口和验证码注意保持与task_master.py设置的完全一致:
-m = MyQueueManager(address=(server_addr, 5000), authkey=b'abc')
+m = MyQueueManager(address=(server_addr, 41000), authkey=b'abc')
 # 从网络连接:
 m.connect()
 # 获取Queue的对象:
@@ -43,12 +43,14 @@ result = m.get_result_queue()
 # 从task队列取任务,并把结果写入result队列:
 for i in range(10):
     try:
+        # 多个work进程存在时,在结束时,get方法调用异常,无法正常退出进程.
+        # 不是报empty异常.是报 raise EOFError错误.
         n = task.get(timeout=1)
         print('run task %d * %d...' % (n, n))
         r = '%d * %d = %d' % (n, n, n * n)
         time.sleep(1)
         result.put(r)
-    except queue.Queue.Empty:
+    except queue.Empty:
         print('task queue is empty.')
 # 处理结束:
 print('worker exit.')
