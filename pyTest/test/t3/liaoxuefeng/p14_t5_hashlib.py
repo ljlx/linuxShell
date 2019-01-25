918dd44785bb2a5e1ce37cdff085e07b3a2492e3
hx940929 <836845967@qq.com>
2019-01-25 17:34:38 +0800

    [py],分行读取当前py文件并计算md5值,sha1

--- pyTest/test/t3/liaoxuefeng/p14_t5_hashlib.py
+++ pyTest/test/t3/liaoxuefeng/p14_t5_hashlib.py
@@ -23,6 +23,11 @@ import os
 # 上下文环境,比如当本py文件被调用时,能取到调用时的所在路径,调用的py文件名.
 
 def md5CurrPy():
+    """
+    MD5是最常见的摘要算法，速度很快，生成结果是固定的128 bit字节，
+    通常用一个32位的16进制字符串表示
+    :return:
+    """
     curPyFile = os.getcwd() + "/p14_t5_hashlib.py"
     md5obj = hashlib.md5()
     with open(curPyFile, mode='r+') as curPyFileIO:
@@ -35,10 +40,27 @@ def md5CurrPy():
                 if tmp:
                     print(tmp)
                     indexCount += 1
+                    # 如果数据量很大，可以分块多次调用update()，最后计算的结果是一样的
                     md5obj.update(tmp.encode())
                 else:
                     loop = False
             md5value = md5obj.hexdigest()
             print("读取并计算文件md5结束.共计读取[%s]行,md5[%s]" % (indexCount, md5value))
 
-md5CurrPy()
+
+def shaCurrpy():
+    """
+    SHA1的结果是160 bit字节，通常用一个40位的16进制字符串表示。
+
+比SHA1更安全的算法是SHA256和SHA512，不过越安全的算法不仅越慢，而且摘要长度更长
+    :return:
+    """
+
+    sha1obj=hashlib.sha1()
+    with open(os.getcwd()+"/p14_t5_hashlib.py") as currpyobj:
+        currpytext=currpyobj.read()
+        sha1obj.update(currpytext)
+        print(sha1obj.hexdigest())
+if __name__ == '__main__':
+    md5CurrPy()
+    shaCurrpy()
