8134cc6ca8a43ef855d05c6426ce5ef2352ffbf5
hx940929 <836845967@qq.com>
2019-01-27 23:09:24 +0800

    [py] 学习笔记-imooc网上的图像识别课程,需要使用安装anaconda环境来.

---
+++ pyTest/test/imooc/t1_discernNumber/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# --------------------------------------------------
+# File Name: 
+# Author: hanxu
+# AuthorSite: http://www.thesunboy.com/
+# GitSource: https://github.com/hx940929/linuxShell
+# Created Time: 2019-01-27 下午9:01
+# ---------------------说明--------------------------
+#  
+# ---------------------------------------------------
---
+++ pyTest/test/imooc/t1_discernNumber/mnist/input_data.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# --------------------------------------------------
+# File Name: input_data.py
+# Author: hanxu
+# AuthorSite: http://www.thesunboy.com/
+# GitSource: https://github.com/hx940929/linuxShell
+# Created Time: 2019-1-27-下午9:01
+# ---------------------说明--------------------------
+# 下载训练数据集.
+# ---------------------------------------------------
+# 方式 1.从网上下载,导入程序,或工作目录中.
+# 方式 2.使用代码下载训练数据集
+# 学习视频
+# https://www.imooc.com/video/17186
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# import gzip
+# import os
+# import tempfile
+# import numpy
+# from six.moves import urllib
+# from six.moves import xrange
+# import tensorr
+
+print()
