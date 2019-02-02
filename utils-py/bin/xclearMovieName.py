0f0d1a5495c86c537c045811c05c5770f10dedd8
hx940929 <836845967@qq.com>
2019-02-02 23:12:30 +0800

    [py]-工具系列-使用正则实现清除文件名无意义字符

--- utils-py/bin/xclearMovieName.py
+++ utils-py/bin/xclearMovieName.py
@@ -12,11 +12,125 @@
 
 # 1.加载指定的各个目录.
 # 2.z;
+import os
+
 
 def getCurDir():
-    import os
+    """
+    获取程序执行调用时的目录.
+    :return:
+    """
     pwdstr = os.getcwd()
-    print(pwdstr)
+    return pwdstr
 
 
-getCurDir()
+def initSearchDir():
+    """
+    查找需要改名的目录,并返回目录列表
+    :return:
+    """
+    pydir = []
+    # 1.手动添加搜索的目录
+    pydir.append("/media/hanxu/d/TDDOWNLOAD")
+    return pydir
+
+
+# 数据清洗链.
+def filterChain(movieList):
+    """
+    清洗,规则匹配的返回true
+    :return:
+    """
+    lastlist = movieList
+    for keyname, filter in filterDict.items():
+        lastlist = filter(lastlist)
+        print("使用过滤器[%s]进行数据清洗过滤." % (keyname))
+    return lastlist
+
+
+filterDict = {}
+
+
+def initFilter():
+    """
+    使用面向过程方法编程.
+    向过滤链添加后缀名过滤
+    :return:
+    """
+    filterDict['suffix'] = filter_suffix
+    filterDict['rmbracket'] = filter_bracket
+    # 1.手动加载过滤规则.
+
+
+def filter_suffix(movieList):
+    """
+    过滤器-匹配后缀是,mkv,rmvb等
+    :return:
+    """
+    # (.+.sh$)|(.+.py$)
+    # .+.(py|sh)$
+    import re
+    re_suffix = re.compile(r'.+.(avi|mkv|mp4|rmvb|gpg)$')
+
+    def dofilter(movie):
+        matchResult = re_suffix.match(movie)
+        if matchResult:
+            return True
+        return False
+
+    # 过滤链如果处理, 这里不使用list. 后续一并多个filter计算
+    return list(filter(dofilter, movieList))
+
+
+def filter_bracket(movielist: list):
+    """
+    移除电影天堂相关不需要的字符串.
+    :param movielist:
+    :return:
+    """
+    import re
+    """
+    该表达式匹配'[]'括号及其括号里的内容,支持同时存在多个元素.
+    """
+    # re_bracket = re.compile(r'(\[\w+\])|(\[.+\])')
+    # re_bracket = re.compile(r'(\[\w+\])|(\[((\w+|\.+)+)\])')
+    # 测试字符串:
+    # [sss-www.dy2018.com]testBDttt.rmvb
+    # [ssswww.dy2018.com]testBDttt.rmvb
+    # [asdf]test.mkv
+    # [.]test.mkv
+    # [sss].moviename.[asdf]
+    # [xxxwww.dygod.com].[xxxwww.dygod.com].fff.[ss.1024ss].rmvb
+    # [xxxwww.dygod.com]fff[ss.1024ss].rmvb
+    re_bracket = re.compile(r'(\.?\[((\w+|\.+|\-+)+)\])')
+
+    def reduce(movie: str):
+        originalName = movie
+        if "最后的武士" in movie:
+            print("")
+            pass
+        bracketResult = re_bracket.findall(movie)
+        if bracketResult:
+            for item in bracketResult:
+                if isinstance(item, tuple):
+                    for item2 in item:
+                        movie = movie.replace(item2, "")
+                elif isinstance(item, str):
+                    movie = movie.replace(item, "")
+            print("文件 %s ==> %s" % (originalName, movie))
+        return movie
+
+    return list(map(reduce, movielist))
+
+
+def init():
+    initFilter()
+
+
+if __name__ == '__main__':
+    init()
+    curdir = getCurDir()
+    filelist = os.listdir(curdir)
+    filelist = filterChain(filelist)
+    for item in filelist:
+        print(item)
