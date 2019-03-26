540c2cc85e9e4961c15cad35c83896e808fde6e3
hanxu <940274999@qq.com>
2019-03-26 22:02:54 +0800

    [go] 书中例子编写

---
+++ goTest/src/t2/p5_stacker/main.go
@@ -0,0 +1,9 @@
+package p5_stacker
+
+import "t2/p5_stacker/stack"
+
+func main() {
+	var  ss stack.Stack
+	ss.Len()
+	//var haystack = stack.Stack
+}
---
+++ goTest/src/t2/p5_stacker/stack/stack.go
@@ -0,0 +1,11 @@
+package stack
+
+type Stack []interface{}
+
+func (stack Stack) Len() int {
+	return len(stack)
+}
+
+func asdf(ss string) {
+
+}
