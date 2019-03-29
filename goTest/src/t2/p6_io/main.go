8020f34c25be2c997c520b6d1cec5c31e6dd40bc
hanxu <940274999@qq.com>
2019-03-29 13:37:10 +0800

    [go]-io例子打通.

--- goTest/src/t2/p6_io/main.go
+++ goTest/src/t2/p6_io/main.go
@@ -8,7 +8,8 @@ import (
 	"bufio"
 	"regexp"
 	"path/filepath"
-	"errors"
+	"strings"
+	"io/ioutil"
 )
 
 /**
@@ -74,6 +75,10 @@ func test1() {
 
 var britishAmerican = "british-american.txt"
 
+/**
+(err error)具名返回值,
+在使用具名返回值时有一个作用域的细节
+ */
 func americanise(inFile io.Reader, outFile io.Writer) (err error) {
 
 	//使用装饰器模式对io对象进行修饰.类似java的io实现
@@ -84,10 +89,12 @@ func americanise(inFile io.Reader, outFile io.Writer) (err error) {
 	//TO-DO 还不太明白这个, 类似py的内部def?
 	//defer 在americanise函数退出后执行.
 	//创建一个匿名的延迟函数，它会在americanise()函数返回并将控制权交给其调用者之前刷新writer的缓冲
+	//TODO 如果有多个defer,执行顺序?
 	defer func() {
+		//如果想忽略任何在刷新操作之前或者在刷新操作过程中发生的任何错误，可以简单地调用defer writer.Flush()，但是这样做的话程序对错误的防御性将较低
 		println("函数hook start...")
 		if err == nil {
-
+			//由于刷新缓冲区操作也可能会失败，所以我们将 writer.Flush()函数的返回值赋值给err
 			err = writer.Flush()
 		} else
 		{
@@ -99,15 +106,20 @@ func americanise(inFile io.Reader, outFile io.Writer) (err error) {
 		println("函数hook end...")
 	}()
 
-	//类似lambda表达式?
+	//我们有时必须在赋值时先声明一个变量，如这里的replacer变量（标识①）和我们这里读入的line变量（标识②）。另一种可选的方式是显式地返回所有返回值，就像我们在其他地方所做的那样
 	var replacer func(string) string
+	//每发现一个匹配的值就调用一次 replacer 函数，并将该匹配到的文本内容替换为replacer函数返回的文本内容。
 
 	if replacer, err = makeReplacerFunc(britishAmerican); err != nil {
-
+		//但是如果我们在函数内部某个地方使用了if value :=...这样的语句，因为if语句会创建一个新的块，所以这个value是一个新的变量，它会隐藏掉名字同为value的具名返回值。
+		//因此我们必须保证不使用快速变量声明符:=来为其赋值，以避免意外创建出一个影子变量
 		return err
 
 	}
 	//使用正则匹配一个单词(用空格作为单词分隔符)
+	//这个函数比较适合于正则表达式内容是从外部文件读取或由用户输入的场景，因为需要做一些错误处理。
+	//regexp.Compile("[A-Za-z]+") return *regex,err
+	//这个must会抛异常
 	wordRx := regexp.MustCompile("[A-Za-z]+")
 
 	//loop 条件, 类似循环是否读取到末尾,可以作为一个工具类封装起来,这点没有py方便.
@@ -117,7 +129,7 @@ func americanise(inFile io.Reader, outFile io.Writer) (err error) {
 	for !eof {
 
 		var line string
-
+		//reader.ReadLine
 		line, err = reader.ReadString('\n')
 
 		if err == io.EOF {
@@ -133,7 +145,9 @@ func americanise(inFile io.Reader, outFile io.Writer) (err error) {
 		}
 
 		line = wordRx.ReplaceAllStringFunc(line, replacer)
-
+		//如果我们有一个非常小的replacer 函数，比如只是简单地将匹配的字母转换成大写，我们可以在调用替换函数的时候将其创建为一个匿名函数,这类似与一个lambda表达式.
+		line = wordRx.ReplaceAllStringFunc(line, func(word string) string { return strings.ToUpper(word) })
+		//我们在这里使用了空标记符_（标识③）。这里的空标记符作为一个占位符放在需要一个变量的地方，并丢弃掉所有赋给它的值。空占位符不是一个新的变量，因此如果我们使用:=，至少需要声明一个其他的新变量
 		if _, err = writer.WriteString(line); err != nil {
 
 			return err
@@ -145,9 +159,34 @@ func americanise(inFile io.Reader, outFile io.Writer) (err error) {
 	return nil
 
 }
-func makeReplacerFunc(s string) (func(string) string, error) {
-	err := errors.New("not impl")
-	return nil, err
+func makeReplacerFunc(file string) (func(string) string, error) {
+	//err := errors.New("not impl")
+	rawBytes, err := ioutil.ReadFile(file)
+
+	if err != nil {
+		return nil, err
+	}
+	text := string(rawBytes)
+	//完全看不懂这一些奇奇怪怪的字符/xk/px
+	usForBritish := make(map[string]string)
+	lines := strings.Split(text, "\n")
+	//range xxx 相当于循环这个集合的长度的次数.
+	for _, lineItem := range lines {
+		//TODO
+		fields := strings.Fields(lineItem)
+		//Todo why is 2 ?
+		if len(fields) == 2 {
+			usForBritish[fields[0]] = fields[1]
+		}
+	}
+	return func(word string) string {
+		if usWord, found := usForBritish[word]; found {
+			return usWord
+		} else {
+			return word
+		}
+	}, nil
+	//return nil, err
 }
 
 /**
@@ -209,17 +248,22 @@ func testInterfaceReader(reader io.Reader) []byte {
 	//bufwriter := func(b2 []byte) io.Writer {
 	//TODO 应该如何把这个if里面产生的twriter值向下传递到read块(if语句里的read代码块)中
 	//如何像java那样定义一个 io.writer buufwriterObj = null;
-	bufwriter := func() io.Writer {
-		if tmpfile, err := os.OpenFile("/tmp/hosts", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0600); err == nil {
-			twriter := bufio.NewWriter(tmpfile)
-			twriter.WriteString("fuck 怎么连初始化变量,吧这个writer带出去都不懂/xk/wn/px \r\n")
-			//twriter.Write(b2)
-			twriter.Flush()
-			return twriter
-		}
-		return nil
+	//var bufwriterobj bufio.Writer
+	var bufwriterobj io.Writer
+	//bufwriter := func() io.Writer {
+	if tmpfile, err := os.OpenFile("/tmp/hosts", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0600); err == nil {
+		twriter := bufio.NewWriter(tmpfile)
+		twriter.WriteString("fuck 怎么连初始化变量,吧这个writer带出去都不懂/xk/wn/px \n")
+		//twriter.Write(b2)
+		twriter.Flush()
+		//TODO 如何强制转型?
+		bufwriterobj = twriter
+		//bufwriterobj = (bufio.Writer)twriter
+		//bufwriterobj.Write([]byte{0})
+		//return twriter
 	}
-
+	//return nil
+	//}
 
 	for isloop {
 		//Read()方法从调用该方法的值中读取数据，并将其放到一个字节类型的切片(b)中
@@ -231,7 +275,8 @@ func testInterfaceReader(reader io.Reader) []byte {
 			size += 1
 			//TODO 不太明白这个内部func的作用和使用方法
 			//我应该如何实现,在这里的read出来的字节,在写入上面创建的writer对象流中,(不使用函数签名和返回writer对象的方法.)
-			bufwriter().Write(b)
+			bufwriterobj.Write(b)
+			bufio.NewWriter(bufwriterobj).Flush()
 
 		} else {
 			if err == io.EOF {
@@ -243,6 +288,7 @@ func testInterfaceReader(reader io.Reader) []byte {
 			println(size)
 		}
 	}
+
 	//TODO 如何将字节转化为字符串?
 	return b
 }
