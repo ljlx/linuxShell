8191a1658b9516c10bafa215426cfae0f23503ee
hx940929 <836845967@qq.com>
2019-04-02 23:25:32 +0800

    [go]. 字符串学习和ascii.

--- goTest/src/t1/p4_strings/main.go
+++ goTest/src/t1/p4_strings/main.go
@@ -13,7 +13,12 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+	"strconv"
+	"bytes"
+)
 
 func testForString() {
 	//	一个 Go语言字符串是一个任意字节的常量序列
@@ -34,6 +39,7 @@ func testString() {
 	//3 所有的ascii字符,在go中可以使用切片[]操作访问具体的一个字节.
 	text0 := "hello"
 	text1 := "hel我lo"
+	text3 := "a测a"
 	//104 101 在ascii字码表上, 映射为 h和e, 验证了2和3.
 	fmt.Println(text1[0], text1[1])
 	//108 230 136 145 108. 在ascii字码表上是 字母'l' , 说明230 136 145 代表utf8编码下的'我'
@@ -54,7 +60,61 @@ func testString() {
 	fmt.Println("字符[a,b,c]的大小关系:", char_a < char_b, char_a < char_c, char_a < char_b)
 	fmt.Println("字符[a,b,c]的大小关系:", char_a == char_b, char_a < char_c, char_a > char_b)
 	fmt.Println("字符[a,b,c]的大小关系:", char_a-char_b, char_a+char_c, char_a*char_b)
+	fmt.Printf("字符串[%s]转换Unicode码点[%d] \n", text3, []rune(text3))
+	//可以使用更快的
+	fmt.Printf("更快的字符串[%s]的字符数[%d]", text3, utf8.RuneCountInString(text3))
+	fmt.Printf("字符串[%s]的字节数[%d],字符数[%d] \n", text3, len(text3), len([]rune(text3)))
+	fmt.Printf("字符串[%s]的原始字节切片数组[%d],无副本地字节转字符串[%s] \n", text3, []byte(text3), string([]byte(text3)))
+	fmt.Printf("将任意类型的数字i:[%d],转成字符串[%s],假设i是一个Unicode码点. \n", 99, string(99))
+	allAsciiChar := ""
+	for i := 1; i <= 200; i++ {
+		allAsciiChar += string(i)
+	}
+	fmt.Printf("从0-200的ascii字符集合:%s \n", allAsciiChar)
+	fmt.Printf("数字 [%d],tostring()==> [%s] \n", 88, strconv.Itoa(88))
+	fmt.Sprint(44)
+	fmt.Sprintf("任意类型x的字符串表示,相当于tostring(),其调用了类型的String()方法,数字:[%d],返回值为:%s", 66, 66)
 
+}
+
+func testChar2String() {
+	allchar := ""
+	//var allchar2 []string = nil
+	allchar_rune := []rune{'æ', 0xE6, 0346, 230, '\xE6', '\u00E6'}
+	for _, char := range allchar_rune {
+		fmt.Printf("[0x%X '%c']", char, char)
+		allchar += string(char)
+
+	}
+	fmt.Println(allchar)
+	fmt.Println("##---------------------分隔符-------------------------")
+	//虽然方便，但是使用+= 操作符并不是在一个循环中往字符串末尾追加字符串最有效的方式。
+	//TODO 方式一: 一个更好的方式（Python程序员可能非常熟悉）是准备好一个字符串切片（[]string），然后使用strings.Join()函数一次性将其中所有字符串串联起来。
+	// 如何将一个码点切片快速转成一个[]string,不用循环+=.
+	//strings.Join(allchar2,)可迭代的一个string?
+	//strings.Join(allchar_rune," ")
+	str_charrune := string(allchar_rune)
+	//strings.Join(str_charrune,"")
+	fmt.Println(str_charrune)
+	//方式二:  但在Go语言中还有一个更好的方法，其原理类似于Java中的StringBuilder。这里有个例子。
+	var buffer bytes.Buffer
+	for {
+		if piece, ok := getNextValidString(); ok {
+			buffer.WriteString(piece)
+		} else {
+			break
+		}
+	}
+
+	fmt.Print(buffer.String(), "\n")
+
+	fmt.Println("###testChar2String-------------------------分隔符-----------------")
+	//使用字符串创建一个rune(码点)切片,
+	//变量chars的类型为 []int32, 因为rune是int32的同义词.
+	chars := []rune("hello,world")
+	//	同时需要在解析过程中能查看前一个或后一个字符时会有用。相反的转换也同样简单，其语法为S:=string(chars)
+	chars2str := string(chars)
+	fmt.Println(chars2str)
 }
 
 func main() {
@@ -62,6 +122,6 @@ func main() {
 
 	testForRange("test1", "test2", "test34", "test5")
 	testString()
-
+	testChar2String()
 	fmt.Println("main end...")
 }
