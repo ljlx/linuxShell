ff33de11c9b9af89876a52f0c6620183f0897ebd
hx940929 <836845967@qq.com>
2019-04-16 21:20:20 +0800

    [shell] killAll脚本加入用户手动控制.

--- utils-shells/bin/killALL.sh
+++ utils-shells/bin/killALL.sh
@@ -14,8 +14,68 @@ if [[ -z ${signal} ]]; then
     signal=15;
 
 fi
-pid=$(ps aux | grep $pname | grep -v grep | awk '{print $2}' | xargs);
+#grep -v $0 过滤自身脚本.
+
+pid=$(ps aux | grep $pname | grep -v grep |grep -v $0| awk '{print $2}' | xargs);
 
 echo "使用kill -${signal} 来杀死。${pid}";
+/bin/ps -p ${pid}
+for item in ${pid[@]}; do
+#    readarray -t pidlist
+#    echo $(/bin/ps -p ${item})
+    isright=1
+    while true; do
+        read -p "是否结束进程[${item}]? default:Y (Y/N): " isdelete
+        if [[ ${isdelete} == "y" || ${isdelete} == "Y" ]]; then
+            echo "结束进程(kill -${signal} $item)"
+            kill -${signal} ${item}
+            break
+        elif [[ ${isdelete} == "n" || ${isdelete} == "N" ]]; then
+#            echo "跳过进程:${item}"
+            break
+        else
+            # 非法输入
+            echo "确认结束请按Y(y) ,否则N(n)"
+    fi
+    done
+done
+
 #xargs
-kill -${signal} ${pid}
+#kill -${signal} ${pid}
+
+
+
+
+
+
+function setconf() {
+    local i=1
+
+    for element in "$@"
+    do
+        IFS=':', read -ra array <<< "$element"
+        if [[ "${array[0]}" = "infile" ]]; then
+            local input=${array[1]}
+            continue
+        elif [[ "${array[0]}" = "outfile" ]]; then
+            local output=${array[1]}
+            continue
+        fi
+        para[$i]="-e s#${array[0]}#${array[1]}#g "
+        para+=${para[$i]}
+        i=$(($i + 1))
+    done
+    if [[ -n "$output" ]]; then
+        cp $output ${output}.org
+        sed $para $input > $output
+    else
+        echo "ss"
+        echo $array
+#        cp $input ${input}.org
+#        sed -i $para $input
+    fi
+
+    read -p "asdfasdf" asdf
+    echo $asdf
+}
+
