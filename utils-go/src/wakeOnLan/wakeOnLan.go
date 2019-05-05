b1f9e88a0745a69a470fb9a24e9e488bdd7849d8
hx940929 <836845967@qq.com>
2019-05-06 00:27:06 +0800

    [go] 使用go实现wakeOnlan功能,并进行高阶函数测试实现.

---
+++ utils-go/src/wakeOnLan/wakeOnLan.go
@@ -0,0 +1,21 @@
+/*
+--------------------------------------------------
+ File Name: wakeOnLan.go
+ Author: hanxu
+ AuthorSite: http://www.thesunboy.com/
+ GitSource: https://github.com/thesunboy-com/linuxShell
+ Created Time: 2019-5-5-下午11:11
+---------------------说明--------------------------
+ wake on lan
+---------------------------------------------------
+*/
+
+package main
+
+import (
+	"wakeOnLan/wakeonlanLib"
+)
+
+func main() {
+	wakeonlanLib.Main()
+}
