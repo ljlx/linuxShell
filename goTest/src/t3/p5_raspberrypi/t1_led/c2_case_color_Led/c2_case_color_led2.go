343741eb06c12fbd7f0ab926fb22b803741e160d
hanxu <940274999@qq.com>
2019-05-09 18:22:41 +0800

    [go] 放弃nathan-osman这个包. 使用periph包 成功点亮双色led灯.

--- goTest/src/t3/p5_raspberrypi/t1_led/c2_case_color_Led/c2_case_color_led2.go
+++ goTest/src/t3/p5_raspberrypi/t1_led/c2_case_color_Led/c2_case_color_led2.go
@@ -19,20 +19,48 @@ import (
 	"periph.io/x/periph/conn/gpio"
 	"periph.io/x/periph/host"
 	"periph.io/x/periph/host/rpi"
+	"os"
 )
 
-func Case2Main() {
+/*
+ // Low represents 0v.
+    Low Level = false
+    // High represents Vin, generally 3.3v or 5v.
+    High Level = true
+ */
+
+var (
+	loginfo = log.New(os.Stdout, "[info]:", log.LstdFlags)
+	logerr  = log.New(os.Stderr, "[err]:", log.LstdFlags)
+)
+
+func test1() {
 	// 加载所有驱动
 	if _, err := host.Init(); err != nil {
 		log.Fatal(err)
 	}
+	// ----------start----------我加的测试代码----------start----------
+	dutystr := "50%"
+	duty, err := gpio.ParseDuty(dutystr)
+	if err != nil {
+		log.Fatalf("ParseDuty duty fail %v", dutystr)
+	}
+	if duty.Valid() {
+		log.Printf("valu==>DescInfo")
+		
+	}
+	// ----------end------------我加的测试代码----------end------------
 	
 	ticker := time.NewTicker(500 * time.Millisecond)
 	// 针脚11， = bcm283x.GPIO17
 	redLed := rpi.P1_11
+	
 	// 针脚12， = bcm283x.GPIO18
 	greenLed := rpi.P1_12
+	// l=!l, 对l进行取反赋值, 相当于进行开关的交替进行,聪明.
 	for l := gpio.Low; ; l = !l {
+		loginfo.Printf("%v==> \n", l)
+		
 		if err := redLed.Out(l); err != nil {
 			log.Fatal(err)
 		}
@@ -41,4 +69,64 @@ func Case2Main() {
 		}
 		<-ticker.C
 	}
+}
+
+func test2() {
+	led_red := rpi.P1_11   // red gpio-17  ==> P1_11
+	led_green := rpi.P1_12 // green gpio-18  ==> P1_12
+	led_blue := rpi.P1_32  // blue gpio-12  ==> P1_32
+	// 初始化为低电位, 0 ,在这里是false
+	curr := gpio.Low
+	var (
+		err error
+	)
+	err = led_red.Out(curr)
+	err = led_green.Out(curr)
+	err = led_blue.Out(curr)
+	if err != nil {
+		log.Fatalf("%v \n ", err)
+	}
+	// ----------start----------先开始自检----------start----------
+	
+	for x := 0; x <= 3; x++ {
+		err = led_red.Out(gpio.High)
+		err = led_green.Out(gpio.Low)
+		err = led_blue.Out(gpio.Low)
+		if err != nil {
+			log.Fatalf("%v \n ", err)
+		}
+	}
+	loginfo.Printf("质检完成..==> \n")
+	
+	// ----------end------------先开始自检----------end------------
+	
+	unitsize := time.Duration(500)
+	for {
+		//
+		loginfo.Printf("test2==>开启红灯 \n")
+		err = led_red.Out(gpio.High)
+		time.Sleep(time.Millisecond * unitsize)
+		//
+		loginfo.Printf("test2==>开启绿灯 \n")
+		err = led_green.Out(gpio.High)
+		time.Sleep(time.Millisecond * unitsize)
+		//
+		loginfo.Printf("test2==>开启蓝灯 \n")
+		err = led_blue.Out(gpio.High)
+		time.Sleep(time.Millisecond * unitsize)
+		//
+		if err != nil {
+			log.Fatalf("%v \n ", err)
+		}
+	}
+	
+}
+
+func Case2Main() {
+	// 加载所有驱动
+	// if _, err := host.Init(); err != nil {
+	// 	log.Fatal(err)
+	// }
+	test1()
+	test2()
 }
